b7edfa4c3ef1deef2193ddbbd1481ec841990707
cixue <cixue.tsu@gmail.com>
2023-08-22 09:36:32 -0700

    Implement opacity parser and interpolation

--- gotham_spectral_pipeline/zenith_opacity.py
+++ gotham_spectral_pipeline/zenith_opacity.py
@@ -1,10 +1,157 @@
 import datetime
+import pathlib
+import re
+import typing
 
+import loguru
 import numpy
 import numpy.typing
+import scipy.interpolate
 
 
 class ZenithOpacity:
+
+    class Database:
+        InterpolationFunction = typing.Callable[
+            [numpy.typing.NDArray[numpy.floating]], numpy.typing.NDArray[numpy.floating]
+        ]
+
+        path: pathlib.Path
+        loaded: bool
+
+        _time: numpy.typing.NDArray[numpy.floating]
+        _frequency: numpy.typing.NDArray[numpy.floating]
+        _opacity: numpy.typing.NDArray[numpy.floating]
+        _interpolation_functions: dict[tuple[float, float], list[InterpolationFunction]]
+
+        def __init__(self, path: pathlib.Path):
+            self.path = path
+            self.loaded = False
+
+        def _load(self):
+            self._time = numpy.loadtxt(self.path / "time.txt")
+            self._frequency = numpy.loadtxt(self.path / "frequency.txt") * 1e9
+            self._opacity = numpy.loadtxt(self.path / "opacity.txt", unpack=True)
+            self._interpolation_functions = dict()
+            for line in open(self.path / "frequency_lists.txt", "r"):
+                start = float(line.split(maxsplit=1)[0]) * 1e9
+                stop = float(line.rsplit(maxsplit=1)[-1]) * 1e9
+                lidx = numpy.searchsorted(self._frequency, start, side="left")
+                ridx = numpy.searchsorted(self._frequency, stop, side="right")
+                self._interpolation_functions[start, stop] = [
+                    scipy.interpolate.CubicSpline(
+                        self._frequency[lidx:ridx], opacity[lidx:ridx]
+                    )
+                    for opacity in self._opacity
+                ]
+
+            # Convert time from MJD to unix timestamp
+            self._time = (
+                self._time * 86400
+                + datetime.datetime(
+                    year=1858,
+                    month=11,
+                    day=17,
+                    hour=0,
+                    minute=0,
+                    second=0,
+                    tzinfo=datetime.timezone.utc,
+                ).timestamp()
+            )
+
+            self.loaded = True
+
+        @property
+        def time(self) -> numpy.typing.NDArray[numpy.floating]:
+            if not self.loaded:
+                self._load()
+            return self._time
+
+        @property
+        def frequency(self) -> numpy.typing.NDArray[numpy.floating]:
+            if not self.loaded:
+                self._load()
+            return self._frequency
+
+        @property
+        def opacity(self) -> numpy.typing.NDArray[numpy.floating]:
+            if not self.loaded:
+                self._load()
+            return self._opacity
+
+        @property
+        def interpolation_functions(
+            self,
+        ) -> dict[tuple[float, float], list[InterpolationFunction]]:
+            if not self.loaded:
+                self._load()
+            return self._interpolation_functions
+
+        def get_opacity(
+            self, timestamp: float, frequency: numpy.typing.ArrayLike
+        ) -> numpy.typing.NDArray[numpy.floating]:
+            tidx = int(numpy.searchsorted(self.time, timestamp, side="left"))
+            if self.time[tidx] == timestamp:
+                return self._interpolate_opacity(tidx, frequency)
+            left = self._interpolate_opacity(tidx - 1, frequency)
+            right = self._interpolate_opacity(tidx, frequency)
+            left_weight = self.time[tidx] - timestamp
+            right_weight = timestamp - self.time[tidx - 1]
+            total_weight = self.time[tidx] - self.time[tidx - 1]
+            return (left * left_weight + right * right_weight) / total_weight
+
+        def _interpolate_opacity(
+            self, tidx: int, frequency: numpy.typing.ArrayLike
+        ) -> numpy.typing.NDArray[numpy.floating]:
+            frequency_array = numpy.array(frequency, dtype=float)
+            opacity_array = numpy.full_like(frequency, numpy.nan)
+            for (
+                start,
+                stop,
+            ), interpolation_functions in self.interpolation_functions.items():
+                lidx = numpy.searchsorted(frequency_array, start, side="left")
+                ridx = numpy.searchsorted(frequency_array, stop, side="right")
+                if lidx < ridx:
+                    opacity_array[lidx:ridx] = interpolation_functions[tidx](
+                        frequency_array[lidx:ridx]
+                    )
+            return opacity_array
+
+    path: pathlib.Path
+    databases: dict[tuple[int, int], Database]
+
+    def __init__(self, path: str):
+        self.path = pathlib.Path(path)
+        self.databases = dict()
+
+        timestamp_matcher = re.compile(r"Forecasts_(\d+)_(\d+)")
+        for subdirectory in self.path.glob("Forecasts_*_*"):
+            if subdirectory.is_file():
+                continue
+
+            result = timestamp_matcher.match(subdirectory.name)
+            if result is None:
+                loguru.logger.warning(
+                    f"Cannot parse timestamps from {subdirectory = }. Ignoring..."
+                )
+                continue
+
+            start, stop = map(int, result.groups(1))
+            self.databases[start, stop] = ZenithOpacity.Database(subdirectory)
+
+    def get_database(self, timestamp: float) -> Database | None:
+        for (start, stop), database in self.databases.items():
+            if start <= timestamp <= stop:
+                return database
+        return None
+
+    def get_opacity(
+        self, timestamp: float, frequency: numpy.typing.ArrayLike
+    ) -> numpy.typing.NDArray[numpy.floating]:
+        database = self.get_database(timestamp)
+        if database is None:
+            return numpy.full_like(frequency, numpy.nan)
+        return database.get_opacity(timestamp, frequency)
 
     @staticmethod
     def generate_cleo_command(
