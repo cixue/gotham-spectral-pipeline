1c3a61ab9170669d7b92d354cf78cbad8e89d404
cixue <cixue.tsu@gmail.com>
2023-10-01 23:38:27 -0700

    Implement minimum baseline fitting

--- gotham_spectral_pipeline/spectrum.py
+++ gotham_spectral_pipeline/spectrum.py
@@ -1,3 +1,4 @@
+import collections
 import enum
 import functools
 import typing
@@ -29,6 +30,94 @@ def _fit_polynomial_baseline(
     return polynomial, {"polynomial": polynomial}
 
 
+def _auto_fit_polynomial_baseline(
+    frequency: numpy.typing.NDArray[numpy.floating],
+    intensity: numpy.typing.NDArray[numpy.floating],
+    noise: numpy.typing.NDArray[numpy.floating] | None,
+    residual_threshold: float | None,
+    residual_half_moving_window: int,
+    polynomial_options: dict[str, typing.Any],
+) -> tuple[Baseline, BaselineSupplementaryInfo] | None:
+    degree = polynomial_options.pop("degree", None)
+    min_degree = polynomial_options.pop("min_degree", 0)
+    max_degree = polynomial_options.pop("max_degree", 1024)
+
+    if degree is None:
+        if noise is None:
+            loguru.logger.warning(
+                "Finding minimum degree using residual requires noise but this spectrum does not have noise."
+            )
+            return None
+
+        if residual_threshold is None:
+            loguru.logger.warning(
+                "Finding minimum degree using residual requires specifying residual_threshold."
+            )
+            return None
+
+        if min_degree > max_degree:
+            loguru.logger.warning(f"Require {min_degree = } <= {max_degree = }.")
+            return None
+
+        # Find minimum degree such that residual < residual_threshold using binary search.
+        # If no such degree is found, use max_degree.
+        success = False
+        cached_result: tuple[Baseline, BaselineSupplementaryInfo]
+        lb, ub = min_degree, max_degree + 1
+        while lb < ub:
+            degree = lb + (ub - lb) // 2
+            baseline, baseline_info = _fit_polynomial_baseline(
+                frequency, intensity, noise, degree, **polynomial_options
+            )
+            baseline_info["residual"] = residual = _compute_residual(
+                intensity - baseline(frequency),
+                noise,
+                residual_half_moving_window,
+            )
+            if residual < residual_threshold:
+                baseline_info["success"] = success = True
+                cached_result = baseline, baseline_info
+                ub = degree
+            else:
+                if not success:
+                    baseline_info["success"] = success = False
+                    cached_result = baseline, baseline_info
+                lb = degree + 1
+        if not success:
+            degree = max_degree
+            loguru.logger.warning(
+                f"Cannot find a degree <= {max_degree = } such that residual < {residual_threshold = }. Using {degree = }."
+            )
+        baseline, baseline_info = cached_result
+        intensity -= baseline(frequency)
+        return baseline, baseline_info
+    else:
+        baseline, baseline_info = _fit_polynomial_baseline(
+            frequency, intensity, noise, degree, **polynomial_options
+        )
+        intensity -= baseline(frequency)
+
+        if residual_threshold is not None:
+            if noise is None:
+                loguru.logger.warning(
+                    "Computing residual requires noise but this spectrum does not have noise."
+                )
+                return None
+
+            baseline_info["residual"] = residual = _compute_residual(
+                intensity,
+                noise,
+                residual_half_moving_window,
+            )
+            if residual < residual_threshold:
+                baseline_info["success"] = True
+            else:
+                baseline_info["success"] = False
+                loguru.logger.warning(f"{residual = } >= {residual_threshold = }.")
+
+        return baseline, baseline_info
+
+
 def _fit_lomb_scargle_baseline(
     frequency: numpy.typing.NDArray[numpy.floating],
     intensity: numpy.typing.NDArray[numpy.floating],
@@ -55,6 +144,104 @@ def _fit_lomb_scargle_baseline(
     }
 
 
+def _auto_fit_lomb_scargle_baseline(
+    frequency: numpy.typing.NDArray[numpy.floating],
+    intensity: numpy.typing.NDArray[numpy.floating],
+    noise: numpy.typing.NDArray[numpy.floating] | None,
+    residual_threshold: float | None,
+    residual_half_moving_window: int,
+    lomb_scargle_options: dict[str, typing.Any],
+) -> tuple[list[Baseline], list[BaselineSupplementaryInfo]] | None:
+    num_terms = lomb_scargle_options.pop("num_terms", None)
+    min_num_terms = lomb_scargle_options.pop("min_num_terms", 0)
+    max_num_terms = lomb_scargle_options.pop("max_num_terms", 128)
+
+    baseline_list: list[Baseline] = []
+    baseline_info_list: list[BaselineSupplementaryInfo] = []
+
+    if num_terms is None:
+        if noise is None:
+            loguru.logger.warning(
+                "Finding number of terms using residual requires noise but this spectrum does not have noise."
+            )
+            return None
+
+        if residual_threshold is None:
+            loguru.logger.warning(
+                "Finding minimum number of terms using residual which requires specifying residual_threshold."
+            )
+            return None
+
+        if min_num_terms > max_num_terms:
+            loguru.logger.warning(f"Require {min_num_terms = } <= {max_num_terms = }.")
+            return None
+
+        if min_num_terms == 0:
+            residual = _compute_residual(intensity, noise, residual_half_moving_window)
+            if residual < residual_threshold:
+                return [], []
+
+        # Find minimum number of terms such that residual < residual_threshold using linear search.
+        # If no such degree is found, use max_num_terms.
+        success = False
+        for num_terms in range(1, max_num_terms + 1):
+            baseline, baseline_info = _fit_lomb_scargle_baseline(
+                frequency, intensity, noise, **lomb_scargle_options
+            )
+            intensity = intensity - baseline(frequency)
+
+            baseline_info["residual"] = residual = _compute_residual(
+                intensity, noise, residual_half_moving_window
+            )
+            if residual < residual_threshold:
+                baseline_info["success"] = success = True
+            else:
+                baseline_info["success"] = success = False
+
+            baseline_list.append(baseline)
+            baseline_info_list.append(baseline_info)
+
+            if success and num_terms >= min_num_terms:
+                break
+
+        if not success:
+            loguru.logger.warning(
+                f"Cannot find a number of terms <= {max_num_terms = } such that residual < {residual_threshold = }. Using {num_terms = }."
+            )
+        return baseline_list, baseline_info_list
+    else:
+        for _ in range(num_terms):
+            baseline, baseline_info = _fit_lomb_scargle_baseline(
+                frequency, intensity, noise, **lomb_scargle_options
+            )
+            intensity -= baseline(frequency)
+
+            if residual_threshold is not None:
+                if noise is None:
+                    loguru.logger.warning(
+                        "Computing residual requires noise but this spectrum does not have noise."
+                    )
+                    return None
+
+                baseline_info["residual"] = residual = _compute_residual(
+                    intensity,
+                    noise,
+                    residual_half_moving_window,
+                )
+                if residual < residual_threshold:
+                    baseline_info["success"] = True
+                else:
+                    baseline_info["success"] = False
+
+            baseline_list.append(baseline)
+            baseline_info_list.append(baseline_info)
+
+        if not baseline_info_list[-1]["success"]:
+            residual = baseline_info_list[-1]["residual"]
+            loguru.logger.warning(f"{residual = } >= {residual_threshold = }.")
+        return baseline_list, baseline_info_list
+
+
 def _centered_move_sum(
     arr: numpy.typing.NDArray[typing.Any], half_moving_window: int
 ) -> numpy.typing.NDArray[numpy.floating]:
@@ -65,6 +252,32 @@ def _centered_move_sum(
         arr[-moving_window:][::-1], window=moving_window, min_count=1
     )[half_moving_window : 2 * half_moving_window][::-1]
     return move_sum
+
+
+def _centered_move_mean(
+    arr: numpy.typing.NDArray[typing.Any], half_moving_window: int
+) -> numpy.typing.NDArray[numpy.floating]:
+    moving_window = 2 * half_moving_window + 1
+    move_mean = bottleneck.move_mean(arr, window=moving_window, min_count=1)
+    move_mean[:-half_moving_window] = move_mean[half_moving_window:]
+    move_mean[-half_moving_window:] = bottleneck.move_mean(
+        arr[-moving_window:][::-1], window=moving_window, min_count=1
+    )[half_moving_window : 2 * half_moving_window][::-1]
+    return move_mean
+
+
+def _compute_residual(
+    intensity: numpy.typing.NDArray[numpy.floating],
+    noise: numpy.typing.NDArray[numpy.floating],
+    half_moving_window: int,
+):
+    return numpy.nanmean(
+        _centered_move_mean(
+            intensity / noise,
+            half_moving_window,
+        )
+        ** 2
+    )
 
 
 class Spectrum:
@@ -195,6 +408,8 @@ class Spectrum:
         self,
         method: typing.Literal["polynomial", "lomb-scargle"] = "polynomial",
         mask: numpy.typing.NDArray[numpy.bool_] | None = None,
+        residual_threshold: float | None = None,
+        residual_half_moving_window: int = 512,
         polynomial_options: dict[str, typing.Any] = {},
         lomb_scargle_options: dict[str, typing.Any] = {},
     ) -> tuple[Baseline, BaselineSupplementaryInfo] | None:
@@ -203,15 +418,63 @@ class Spectrum:
         intensity = self.intensity[fitted]
         noise = None if self.noise is None else self.noise[fitted]
 
+        if residual_threshold is not None and noise is None:
+            loguru.logger.warning(
+                "Computing residual requires noise but this spectrum does not have noise."
+            )
+            return None
+
+        polynomial_options = polynomial_options.copy()
+        lomb_scargle_options = lomb_scargle_options.copy()
+
+        all_baseline: list[Baseline] = []
+        all_baseline_info: list[BaselineSupplementaryInfo] = []
         if method == "polynomial":
-            return _fit_polynomial_baseline(
-                frequency, intensity, noise, **polynomial_options
+            polynomial_result = _auto_fit_polynomial_baseline(
+                frequency,
+                intensity,
+                noise,
+                residual_threshold,
+                residual_half_moving_window,
+                polynomial_options,
             )
+            if polynomial_result is None:
+                return None
+
+            baseline, baseline_info = polynomial_result
+            all_baseline.append(baseline)
+            all_baseline_info.append(baseline_info)
         elif method == "lomb-scargle":
-            return _fit_lomb_scargle_baseline(
-                frequency, intensity, noise, **lomb_scargle_options
+            lomb_scargle_result = _auto_fit_lomb_scargle_baseline(
+                frequency,
+                intensity,
+                noise,
+                residual_threshold,
+                residual_half_moving_window,
+                lomb_scargle_options,
             )
-        return None
+            if lomb_scargle_result is None:
+                return None
+
+            baseline_list, baseline_info_list = lomb_scargle_result
+            all_baseline.extend(baseline_list)
+            all_baseline_info.extend(baseline_info_list)
+
+        if len(all_baseline) == 1:
+            return all_baseline[0], all_baseline_info[0]
+
+        def combined_baseline(
+            frequency: numpy.typing.NDArray[numpy.floating],
+        ) -> numpy.typing.NDArray[numpy.floating]:
+            if len(all_baseline) == 0:
+                return numpy.zeros_like(frequency)
+            return numpy.sum([baseline(frequency) for baseline in all_baseline], axis=0)
+
+        combined_baseline_info = collections.defaultdict(list)
+        for baseline_info in all_baseline_info:
+            for key, val in baseline_info.items():
+                combined_baseline_info[key].append(val)
+        return combined_baseline, combined_baseline_info
 
     def get_moving_averaged_spectrum(
         self,
