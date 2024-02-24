798402b76b4c16e54d47430e28dafba3540b0c09
cixue <cixue.tsu@gmail.com>
2024-02-24 12:06:29 -0800

    Allow arbitrary arguments to be passed directly to extract_system_temperature

--- scripts/extract_system_temperature.sh
+++ scripts/extract_system_temperature.sh
@@ -14,17 +14,17 @@ function main() {
         return
     fi
 
-    if [[ $# == 0 ]]; then
-        return
-    fi
-    filter=$1; shift
+    local passed_to_gsp=()
+    while [[ $# > 0 && $1 != "--" ]]; do
+        passed_to_gsp+=("$1"); shift
+    done
 
     sdfits=$(${discover_sessions} "$@")
     if [[ $? != 0 ]]; then
         return
     fi
 
-    ${gsp} extract_system_temperature --sdfits ${sdfits[@]} --filter "${filter}"
+    ${gsp} extract_system_temperature "${passed_to_gsp[@]}" --sdfits ${sdfits[@]}
 }
 
 main "$@"
