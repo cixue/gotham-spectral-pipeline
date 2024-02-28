ff9d34bf47434ddb6f2768abded3babcda2b579a
cixue <cixue.tsu@gmail.com>
2024-02-28 02:51:54 -0800

    Add a lru_cache typed wrapper

--- gotham_spectral_pipeline/utils.py
+++ gotham_spectral_pipeline/utils.py
@@ -1,4 +1,32 @@
 import datetime
+import functools
+import typing
+
+__all__ = [
+    "datetime_parser",
+    "lru_cache",
+]
+
+_P = typing.ParamSpec("_P")
+_R = typing.TypeVar("_R")
+
+
+@typing.overload
+def lru_cache(func: typing.Callable[_P, _R], /, **kwargs) -> typing.Callable[_P, _R]:
+    ...
+
+
+@typing.overload
+def lru_cache(
+    **kwargs,
+) -> typing.Callable[[typing.Callable[_P, _R]], typing.Callable[_P, _R]]:
+    ...
+
+
+def lru_cache(func: typing.Callable[_P, _R] | None = None, /, **kwargs):
+    if func is not None:
+        return functools.lru_cache(func, **kwargs)
+    return functools.lru_cache(**kwargs)
 
 
 def datetime_parser(dt: str) -> datetime.datetime:
